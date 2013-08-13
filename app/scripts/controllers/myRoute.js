8c38cddc547d6e251bac6a9d0020520e0501f7cc
Sean Lange <sean@webthingee.com>
2013-08-13 12:07:59 -0700

    clean up for re-use

--- app/scripts/controllers/myRoute.js
+++ app/scripts/controllers/myRoute.js
@@ -4,11 +4,22 @@ myApp
   .controller('myRouteCtrl', function ($scope) {
     $scope.message = 'This is indicates a successful route...';
 
-    $scope.images = [
-      'http://lorempixel.com/400/200/sports/1',
-      'http://lorempixel.com/400/200/sports/2',
-      'http://lorempixel.com/400/200/sports/3',
-      'http://lorempixel.com/400/200/sports/4'
+    $scope.myData = [
+      {
+        "image" : "http://lorempixel.com/1000/400/people/1",
+        "caption" : "Lucas ipsum dolor sit amet darth biggs skywalker binks darth jade."
+        },
+      {
+        "image" : "http://lorempixel.com/1000/400/people/2",
+        "caption" : "Ben binks skywalker kamino moff obi-wan yavin jade."
+        }
+    ];
+
+    $scope.images2 = [
+      'http://lorempixel.com/800/800/food/1',
+      'http://lorempixel.com/700/600/food/2',
+      'http://lorempixel.com/500/700/food/3',
+      'http://lorempixel.com/400/800/food/4'
     ];
   });
 
